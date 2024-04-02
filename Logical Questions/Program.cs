c3f2bce15c7f35d4858142fa5e8ebe4d7b2c582d
mridulthakur <mridul.thakur.007@gmail.com>
2024-04-02 13:06:31 +0530

    Add project files.

---
+++ Logical Questions/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//W.A.P. to reverse a string
+//string str = Console.ReadLine();
+//string empty = "";
+//for(int i = str.Length-1; i>=0; i--)
+//{
+//    empty += str[i];
+//}
+//Console.WriteLine(empty);
+
+//W.A.P. to reverse a elements in an array
+//1st way
+
+int[] arr = new int[5] {10,20,30,40,50};
+int[] newArr = new int[arr.Length];
+int temp;
+int k = 0;
+
+for (int i= arr.Length - 1; i>=0; i--)
+{
+    temp = arr[i];
+    newArr[k] = temp;
+    k++;
+}
+Console.WriteLine(string.Join(",",newArr));
+Console.ReadLine();
+
+//Optimized way
+int[] arr1 = new int[5] { 10, 20, 30, 40, 50 };
+int[] newArr1 = new int[arr.Length];
+int k1 = 0;
+
+for (int i = arr.Length - 1; i >= 0; i--)
+{
+    newArr1[k] = arr1[i];
+
+    k1++;
+}
+Console.WriteLine(string.Join(",", newArr1));
+Console.ReadLine();
