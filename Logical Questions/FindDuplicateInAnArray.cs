29e773f96b2e385c34b57b0ddd875a06816dddfd
mridulthakur <mridul.thakur.007@gmail.com>
2024-04-03 10:01:54 +0530

    Find duplicates in an array

---
+++ Logical Questions/FindDuplicateInAnArray.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Logical_Questions
+//{
+//    internal class FindDuplicateInAnArray
+//    {
+//        //W.A.P. to find Find Duplicate In An Array 
+
+//        int[] arr = new int[5] { 12, 32, 32, 43, 12 };
+
+//for (int i = 0; i <= arr.Length; i++)
+//{
+//    for(int j=i+1; j<=arr.Length-1; j++)
+//    {
+//        if (arr[i] == arr[j])
+//        {
+//            Console.WriteLine(arr[j].ToString());
+//        }
+//}
+//}
+//    }
+//}
--- Logical Questions/Program.cs
+++ Logical Questions/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
 //W.A.P. to reverse a string
@@ -10,33 +10,36 @@
 //}
 //Console.WriteLine(empty);
 
+//################################################################################//
+
 //W.A.P. to reverse a elements in an array
 //1st way
+//int[] arr = new int[5] {10,20,30,40,50};
+//int[] newArr = new int[arr.Length];
+//int temp;
+//int k = 0;
 
-int[] arr = new int[5] {10,20,30,40,50};
-int[] newArr = new int[arr.Length];
-int temp;
-int k = 0;
+//for (int i= arr.Length - 1; i>=0; i--)
+//{
+//    temp = arr[i];
+//    newArr[k] = temp;
+//    k++;
+//}
+//Console.WriteLine(string.Join(",",newArr));
+//Console.ReadLine();
 
-for (int i= arr.Length - 1; i>=0; i--)
-{
-    temp = arr[i];
-    newArr[k] = temp;
-    k++;
-}
-Console.WriteLine(string.Join(",",newArr));
-Console.ReadLine();
+////Optimized way
+//int[] arr1 = new int[5] { 10, 20, 30, 40, 50 };
+//int[] newArr1 = new int[arr.Length];
+//int k1 = 0;
 
-//Optimized way
-int[] arr1 = new int[5] { 10, 20, 30, 40, 50 };
-int[] newArr1 = new int[arr.Length];
-int k1 = 0;
+//for (int i = arr.Length - 1; i >= 0; i--)
+//{
+//    newArr1[k] = arr1[i];
 
-for (int i = arr.Length - 1; i >= 0; i--)
-{
-    newArr1[k] = arr1[i];
+//    k1++;
+//}
+//Console.WriteLine(string.Join(",", newArr1));
+//Console.ReadLine();
 
-    k1++;
-}
-Console.WriteLine(string.Join(",", newArr1));
-Console.ReadLine();
+
